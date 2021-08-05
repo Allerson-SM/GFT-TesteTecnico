d14fe5ca325e8a1241cf08710053000a311233f8
Allerson <allerson.santana354@gmail.com>
2021-08-05 17:51:32 -0300

    Criando a atividade 3

---
+++ Atividade_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a data do seu aniversário - 2020");
+            System.Console.WriteLine("Informe o dia do seu aniversário");
+            var dia = Int32.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Informe o mês do seu aniversário");
+            var mes = Int32.Parse(Console.ReadLine());
+
+            // System.Console.WriteLine("Informe o ano do seu aniversário");
+            // var ano = Console.ReadLine();
+            
+            var date1 = new DateTime(2020, mes, dia);
+            Console.WriteLine(date1);
+        }
+    }
+}
