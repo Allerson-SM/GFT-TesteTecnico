b9063ac1f79a17ce4e928567f0e4bb5866b49f5f
Allerson <allerson.santana354@gmail.com>
2021-08-05 18:02:47 -0300

    Ajuste atividade 3

--- Atividade_3/Program.cs
+++ Atividade_3/Program.cs
@@ -6,18 +6,26 @@ namespace Atividade_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a data do seu aniversário - 2020");
+            Console.WriteLine("aniversário");
+            System.Console.WriteLine();
+            System.Console.WriteLine();
             System.Console.WriteLine("Informe o dia do seu aniversário");
             var dia = Int32.Parse(Console.ReadLine());
 
             System.Console.WriteLine("Informe o mês do seu aniversário");
             var mes = Int32.Parse(Console.ReadLine());
 
-            // System.Console.WriteLine("Informe o ano do seu aniversário");
-            // var ano = Console.ReadLine();
-            
-            var date1 = new DateTime(2020, mes, dia);
-            Console.WriteLine(date1);
+            System.Console.WriteLine("Informe o ano do seu aniversário");
+            var ano = Int32.Parse(Console.ReadLine());
+
+            if (dia > 0 && dia <= 31 && mes > 0 && mes <= 12 && ano == 2020)
+            {
+                System.Console.WriteLine("Data válida");
+            }
+            else
+            {
+                System.Console.WriteLine("Data inválida");
+            }
         }
     }
 }
